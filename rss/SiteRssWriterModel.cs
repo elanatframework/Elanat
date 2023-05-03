4b27ebed5bc7e6822f0d1db658a80f5ee5b91013
elanatframework <111444759+elanatframework@users.noreply.github.com>
2023-05-04 02:03:52 +0330

    Add files via upload

---
+++ rss/SiteRssWriterModel.cs
@@ -0,0 +1,223 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Xml;
+
+namespace elanat
+{
+    public class SiteRssWriterModel
+    {
+        public void SetValue(NameValueCollection QueryString)
+        {
+            // Rss Extra Helper Active Check
+            DataUse.ExtraHelper dueh = new DataUse.ExtraHelper();
+            string RssExtraHelperId = dueh.GetExtraHelperIdByExtraHelperName("rss_option");
+            dueh.FillCurrentExtraHelper(RssExtraHelperId);
+
+            if (!dueh.ExtraHelperActive.ZeroOneToBoolean())
+            {
+                HttpContext.Current.Response.Write(Language.GetLanguage("page_is_inactive", StaticObject.GetCurrentSiteGlobalLanguage()));
+                return;
+            }
+
+
+            // Get Rss Cache
+            string CacheKey = "el_rss";
+
+            if (!string.IsNullOrEmpty(QueryString["site"]))
+                CacheKey += ":" + QueryString["site"].ToString();
+
+            if (!string.IsNullOrEmpty(QueryString["user_id"]))
+                CacheKey += ":" + QueryString["user_id"].ToString();
+
+            if (!string.IsNullOrEmpty(QueryString["category_name"]))
+                CacheKey += ":" + QueryString["category_name"].ToString();
+
+            if (!string.IsNullOrEmpty(QueryString["content_type"]))
+                CacheKey += ":" + QueryString["content_type"].ToString();
+
+            CacheClass cc = new CacheClass();
+
+            if (cc.ExistInDisk(CacheKey))
+            {
+                HttpContext.Current.Response.Write(cc.GetValue("disk", CacheKey));
+                return;
+            }
+
+
+            // Set Value
+            XmlDocument RssOptionDocument = new XmlDocument();
+            RssOptionDocument.Load(HttpContext.Current.Server.MapPath(StaticObject.SitePath + "add_on/extra_helper/rss_option/option/rss_option.xml"));
+
+            XmlNode node = RssOptionDocument.SelectSingleNode("rss_option_root");
+
+            string FeedCount = node["feed_count"].Attributes["value"].Value;
+            int ContentTextCount = int.Parse(node["content_text_length"].Attributes["value"].Value);
+            int RssCache = int.Parse(node["rss_cache"].Attributes["value"].Value);
+            bool UseRemoveHtmlTag = node["use_remove_html_tag"].Attributes["active"].Value == "true";
+            bool ActiveAuthorFeed = node["author_feed"].Attributes["active"].Value == "true";
+            bool ActiveCategoryFeed = node["category_feed"].Attributes["active"].Value == "true";
+            bool ActiveContentTypeFeed = node["content_type_feed"].Attributes["active"].Value == "true";
+
+
+            // Set Struct
+            string RssBoxStruct = StaticObject.StructDocument.SelectSingleNode("struct_root/rss/box").InnerText;
+            string RssListItemStruct = StaticObject.StructDocument.SelectSingleNode("struct_root/rss/list_item").InnerText;
+            string TmpRssListItemStruct = "";
+            string SumRssListItemStruct = "";
+
+
+            // Set Rss Box Struct Value
+            string LastBuildDate = DateAndTime.GetDate("ddd, dd MMM yyyy HH:mm:ss zzz");
+            string CurrentDateYear = DateAndTime.GetDate("yyyy");
+
+            DataUse.Site dus = new DataUse.Site();
+            string SiteDateYear = dus.GetSiteDateCreate(StaticObject.GetCurrentSiteSiteId()).Substring(0, 4);
+
+
+            RssBoxStruct = RssBoxStruct.Replace("$_asp site_date_year;", SiteDateYear);
+            RssBoxStruct = RssBoxStruct.Replace("$_asp current_date_year;", CurrentDateYear);
+            RssBoxStruct = RssBoxStruct.Replace("$_asp last_build_date;", LastBuildDate);
+            RssBoxStruct = RssBoxStruct.Replace("$_lang _rss_title;", Language.GetLanguage("_rss_title", StaticObject.GetCurrentSiteGlobalLanguage()));
+            RssBoxStruct = RssBoxStruct.Replace("$_lang _rss_description;", Language.GetLanguage("_rss_description", StaticObject.GetCurrentSiteGlobalLanguage()));
+            RssBoxStruct = RssBoxStruct.Replace("$_asp language_global_name;", StaticObject.GetCurrentSiteGlobalLanguage());
+            RssBoxStruct = RssBoxStruct.Replace("$_lang copyright;", Language.GetLanguage("copyright", StaticObject.GetCurrentSiteGlobalLanguage()));
+            RssBoxStruct = RssBoxStruct.Replace("$_lang some_rights_reserved;", Language.GetLanguage("some_rights_reserved", StaticObject.GetCurrentSiteGlobalLanguage()));
+            RssBoxStruct = RssBoxStruct.Replace("$_lang elanat_rss_feed;", Language.GetLanguage("elanat_rss_feed", StaticObject.GetCurrentSiteGlobalLanguage()));
+
+
+
+            string InnerAttach = "where el_site.site_id=" + StaticObject.GetCurrentSiteSiteId();
+            string OuterAttach = "order by content_date_create desc,content_time_create desc";
+
+
+            // Set Site Feed
+            if (!string.IsNullOrEmpty(QueryString["site"]) && ActiveAuthorFeed)
+                if (QueryString["site"].ToString().IsNumber())
+                {
+                    string SiteId = dus.GetSiteIdBySiteGlobalName(QueryString["site"].ToString().ProtectSqlInjection());
+
+                    InnerAttach = "where el_site.site_id=" + SiteId;
+                }
+
+            // Set Content Type Feed
+            if (!string.IsNullOrEmpty(QueryString["content_type"]) && ActiveContentTypeFeed)
+            {
+                string ContentType = QueryString["content_type"].ToString().ProtectSqlInjection();
+
+                InnerAttach += " and el_content.content_type='" + ContentType + "'";
+            }
+
+            // Set Author Feed
+            if (!string.IsNullOrEmpty(QueryString["user_id"]) && ActiveAuthorFeed)
+                if (QueryString["user_id"].IsNumber())
+                {
+                    string Author = QueryString["user_id"].ToString().ProtectSqlInjection();
+
+                    InnerAttach += " and el_user.user_id=" + Author;
+                }
+
+            DataUse.Category duc = new DataUse.Category();
+
+            // Set Category Feed
+            if (!string.IsNullOrEmpty(QueryString["category_name"]) && ActiveCategoryFeed)
+            {
+                string CategoryName = QueryString["category_name"].ToString().ProtectSqlInjection();
+
+                string CategoryId = duc.GetCategoryIdByCategoryName(CategoryName);
+
+                if (!string.IsNullOrEmpty(CategoryId))
+                {
+                    InnerAttach += " and el_category.category_id=" + CategoryId;
+                }
+            }
+
+            InnerAttach += " and el_category.category_active=1 and el_content.content_status='active'";
+
+            DataBaseSocket db = new DataBaseSocket();
+            DataBaseDataReader dbdr = new DataBaseDataReader();
+			dbdr.dr = db.GetProcedure("get_content", new List<string>() { "@inner_attach", "@outer_attach", "@count" }, new List<string>() { InnerAttach, OuterAttach, FeedCount.ToString() });
+
+            string ReadMoreRssText = Template.GetSiteTemplate("part/read_more_rss_text");
+            string SitePath = GetCurrentPath() + StaticObject.SitePath;
+            bool whileFirstChecker = true;
+
+            if (dbdr.dr != null && dbdr.dr.HasRows)
+                while (dbdr.dr.Read())
+                {
+                    // Set Extra Content Url Value
+                    ExtraValue evc = new ExtraValue();
+
+                    evc.SiteGlobalName = dbdr.dr["site_global_name"].ToString();
+                    evc.SiteName = dbdr.dr["site_name"].ToString();
+                    evc.ContentId = dbdr.dr["content_id"].ToString();
+                    evc.ContentTitle = dbdr.dr["content_title"].ToString();
+                    evc.CategoryId = dbdr.dr["category_id"].ToString();
+
+                    CategoryClass category = new CategoryClass();
+
+                    evc.ParrentCategory = category.GetParentCategory(dbdr.dr["site_global_name"].ToString(), dbdr.dr["category_id"].ToString());
+                    evc.CategoryName = category.CategoryName;
+
+
+                    TmpRssListItemStruct = RssListItemStruct;
+
+                    // If Content Protection By Password
+                    string ContentText = (string.IsNullOrEmpty(dbdr.dr["content_password"].ToString())) ? dbdr.dr["content_text"].ToString().Replace("<hr class=\"el_read_more\" />", null).Replace("&gt;", ">").Replace("&lt;", "<") : Template.GetSiteTemplate("part/password_protection/protection_content_by_password");
+
+                    if (UseRemoveHtmlTag)
+                    {
+                        ContentText = StringClass.RemoveHtmlTags(ContentText);
+
+                        if (ContentText.Length > ContentTextCount)
+                        {
+                            ContentText = ContentText.Substring(0, ContentTextCount) + ReadMoreRssText;
+                        }
+                    }
+
+                    string ContentPublishDate = string.Format("{0:ddd, dd MMM yyyy HH:mm:ss zzz}", DateTime.Parse(dbdr.dr["content_date_create"].ToString() + " " + dbdr.dr["content_time_create"].ToString()));
+
+                    TmpRssListItemStruct = TmpRssListItemStruct.Replace("$_db content_id;", dbdr.dr["content_id"].ToString());
+                    TmpRssListItemStruct = TmpRssListItemStruct.Replace("$_db content_title;", StringClass.RemoveIllegalCharacters(dbdr.dr["content_title"].ToString()));
+                    TmpRssListItemStruct = TmpRssListItemStruct.Replace("$_db content_text;", StringClass.RemoveIllegalCharacters(ContentText));
+                    TmpRssListItemStruct = TmpRssListItemStruct.Replace("$_asp content_date_publish;", ContentPublishDate);
+                    TmpRssListItemStruct = TmpRssListItemStruct.Replace("$_db user_site_name;", StringClass.RemoveIllegalCharacters(dbdr.dr["user_site_name"].ToString()));
+                    TmpRssListItemStruct = TmpRssListItemStruct.Replace("$_asp site_path;", SitePath);
+
+                    TmpRssListItemStruct = TmpRssListItemStruct.Replace("$_asp extra_content_url_value;", evc.GetContentUrlValue());
+
+                    SumRssListItemStruct += TmpRssListItemStruct;
+
+
+                    // Set Rss Publish Date
+                    if (whileFirstChecker)
+                    {
+                        RssBoxStruct = RssBoxStruct.Replace("$_asp publish_date;", ContentPublishDate);
+                        whileFirstChecker = false;
+                    }
+                }
+
+            db.Close();
+
+            // If Is Not Exist Content
+            RssBoxStruct = RssBoxStruct.Replace("$_asp publish_date;", LastBuildDate);
+
+            string RssContent = RssBoxStruct.Replace("$_asp item;", SumRssListItemStruct);
+
+            HttpContext.Current.Response.Write(RssContent);
+
+            // Set Rss Cache
+            cc.Insert("disk", CacheKey, RssContent, RssCache);
+        }
+
+        private string GetCurrentPath()
+        {
+            string Path = HttpContext.Current.Request.Url.Scheme + @"://" + HttpContext.Current.Request.Url.Authority;
+
+            return Path;
+        }
+    }
+}
---
+++ sitemap/SiteSitemapWriterModel.cs
@@ -0,0 +1,347 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI.WebControls;
+using System.Xml;
+
+namespace elanat
+{
+    public class SiteSitemapWriterModel
+    {
+        public void SetValue(NameValueCollection QueryString)
+        {
+            // Xml Sitemap Extra Helper Active Check
+            DataUse.ExtraHelper dueh = new DataUse.ExtraHelper();
+            string XmlSitemapExtraHelperId = dueh.GetExtraHelperIdByExtraHelperName("xml_sitemap_option");
+            dueh.FillCurrentExtraHelper(XmlSitemapExtraHelperId);
+
+            if (!dueh.ExtraHelperActive.ZeroOneToBoolean())
+            {
+                HttpContext.Current.Response.Write(Language.GetLanguage("page_is_inactive", StaticObject.GetCurrentSiteGlobalLanguage()));
+                return;
+            }
+
+
+            // Get Xml Sitemap Cache
+            string CacheKey = "el_xml_sitemap";
+
+            if (!string.IsNullOrEmpty(QueryString["site"]))
+                CacheKey += ":" + QueryString["site"].ToString();
+
+            CacheClass cc = new CacheClass();
+
+            if (cc.ExistInDisk(CacheKey))
+            {
+                HttpContext.Current.Response.Write(cc.GetValue("disk", CacheKey));
+                return;
+            }
+
+
+            // Set Value
+            XmlDocument XmlSitemapOptionDocument = new XmlDocument();
+            XmlSitemapOptionDocument.Load(HttpContext.Current.Server.MapPath(StaticObject.SitePath + "add_on/extra_helper/xml_sitemap_option/option/xml_sitemap_option.xml"));
+
+            XmlNode XmlSitemapNode = XmlSitemapOptionDocument.SelectSingleNode("xml_sitemap_option_root");
+
+            int XmlSitemapCount = int.Parse(XmlSitemapNode["xml_sitemap_count"].Attributes["value"].Value);
+            int XmlSitemapCache = int.Parse(XmlSitemapNode["xml_sitemap_cache"].Attributes["value"].Value);
+
+
+            string siteMainUrl = GlobalClass.GetSiteMainUrl();
+
+            string SiteGlobalName = "";
+            if (QueryString["site"] != null)
+                SiteGlobalName = QueryString["site"].ToString();
+
+            string SitemapBoxStruct = Struct.GetNode("site_map/sitemap_box").InnerText;
+            string SiteListItemStruct = Struct.GetNode("site_map/list_item/site").InnerText;
+            string TmpSiteListItemStruct = "";
+            string SumSiteListItemStruct = "";
+
+
+            // Set Current Value
+            ListClass lc = new ListClass();
+
+            DataUse.Site dus = new DataUse.Site();
+
+
+            // Set Site Struct
+            if (string.IsNullOrEmpty(SiteGlobalName))
+            {
+                lc.FillSiteGlobalNameListItem();
+
+                foreach (ListItem item in lc.SiteGlobalNameListItem)
+                {
+                    string LastContentDateTimeCreate = dus.GetLastContentDateTimeCreateFromSiteId(item.Value);
+
+                    if (string.IsNullOrEmpty(LastContentDateTimeCreate))
+                        continue;
+
+                    string SiteLastMod = string.Format("{0:yyyy-MM-ddTHH:mm:sszzz}", DateTime.Parse(LastContentDateTimeCreate));
+
+                    TmpSiteListItemStruct = SiteListItemStruct;
+
+                    TmpSiteListItemStruct = TmpSiteListItemStruct.Replace("$_asp site_url_path;", siteMainUrl);
+                    TmpSiteListItemStruct = TmpSiteListItemStruct.Replace("$_asp site_global_name;", item.Text);
+                    TmpSiteListItemStruct = TmpSiteListItemStruct.Replace("$_asp date_time;", SiteLastMod);
+
+                    SumSiteListItemStruct += TmpSiteListItemStruct;
+
+                    XmlSitemapCount--;
+                }
+
+                SitemapBoxStruct = SitemapBoxStruct.Replace("$_asp item;", SumSiteListItemStruct);
+                HttpContext.Current.Response.Write(SitemapBoxStruct);
+
+                return;
+            }
+
+            string SiteId = dus.GetSiteIdBySiteGlobalName(SiteGlobalName);
+
+            if (string.IsNullOrEmpty(SiteId))
+                return;
+
+            string UrlBoxStruct = Struct.GetNode("site_map/url_box").InnerText;
+            string PageListItemStruct = Struct.GetNode("site_map/list_item/page").InnerText;
+            string CategoryListItemStruct = Struct.GetNode("site_map/list_item/category").InnerText;
+            string ContentListItemStruct = Struct.GetNode("site_map/list_item/content").InnerText;
+            string TmpPageListItemStruct = "";
+            string SumPageListItemStruct = "";
+            string TmpCategoryListItemStruct = "";
+            string SumCategoryListItemStruct = "";
+            string TmpContentListItemStruct = "";
+            string SumContentListItemStruct = "";
+
+
+            // Set Page Struct
+            lc.FillSitePageNameListItem(SiteId, StaticObject.GetCurrentSiteGlobalLanguage());
+
+            if ((XmlSitemapCount - lc.SitePageNameListItem.Length) >= 0)
+            {
+                // Set Extra Page Url Value
+                ExtraValue evc = new ExtraValue();
+                CurrentClientObjectClass ccoc = new CurrentClientObjectClass();
+
+                evc.SiteGlobalName = ccoc.SiteSiteGlobalName;
+                evc.SiteName = dus.GetSiteNameBySiteId(ccoc.SiteId);
+
+                DataUse.Page dup = new DataUse.Page();
+
+
+                foreach (ListItem item in lc.SitePageNameListItem)
+                {
+                    evc.PageId = dup.GetPageIdByPageGlobalName(item.Value);
+                    evc.PageTitle = dup.GetPageTitleByPageId(evc.PageId);
+
+                    evc.PageGlobalName = item.Value;
+
+
+                    TmpPageListItemStruct = PageListItemStruct;
+
+                    TmpPageListItemStruct = TmpPageListItemStruct.Replace("$_asp site_url_path;", siteMainUrl);
+                    TmpPageListItemStruct = TmpPageListItemStruct.Replace("$_asp page_global_name;", item.Value);
+                    TmpPageListItemStruct = TmpPageListItemStruct.Replace("$_asp priority;", "1");
+
+                    TmpPageListItemStruct = TmpPageListItemStruct.Replace("$_asp extra_page_url_value;", evc.GetPageUrlValue());
+
+                    SumPageListItemStruct += TmpPageListItemStruct;
+
+                    XmlSitemapCount--;
+                }
+            }
+
+
+            // Set Category Struct
+            CategoryClass cc2 = new CategoryClass();
+            XmlNode CategoryNode = cc2.GetSaveCategoryNode(SiteGlobalName);
+
+            if ((XmlSitemapCount - CategoryNode.ChildNodes.Count) >= 0)
+            {
+                // Set Extra Catgory Url Value
+                ExtraValue evc = new ExtraValue();
+
+                evc.SiteGlobalName = SiteGlobalName;
+
+                DataUse.Site dus2 = new DataUse.Site();
+                evc.SiteName = dus2.GetSiteNameBySiteId(SiteId);
+
+                foreach (XmlNode node in CategoryNode.ChildNodes)
+                {
+                    evc.CategoryId = node.Attributes["id"].Value;
+
+                    evc.ParrentCategory = cc2.GetParentCategory(StaticObject.GetCurrentSiteSiteGlobalName(), node.Attributes["id"].Value);
+                    evc.CategoryName = node.Attributes["name"].Value;
+
+
+                    TmpCategoryListItemStruct = CategoryListItemStruct;
+
+                    TmpCategoryListItemStruct = TmpCategoryListItemStruct.Replace("$_asp site_url_path;", siteMainUrl);
+                    TmpCategoryListItemStruct = TmpCategoryListItemStruct.Replace("$_asp category_name;", node.Attributes["name"].Value);
+                    TmpCategoryListItemStruct = TmpCategoryListItemStruct.Replace("$_asp date_time;", CategoryLastMod(node.Attributes["id"].Value));
+                    TmpCategoryListItemStruct = TmpCategoryListItemStruct.Replace("$_asp change_freq;", GetCategoryChangeFreq(node.Attributes["id"].Value));
+                    TmpCategoryListItemStruct = TmpCategoryListItemStruct.Replace("$_asp priority;", GetSubCategoryPriority(node.Attributes["space"].Value.Length.ToString()));
+
+                    TmpCategoryListItemStruct = TmpCategoryListItemStruct.Replace("$_asp extra_category_url_value;", evc.GetCategoryUrlValue());
+
+
+                    SumCategoryListItemStruct += TmpCategoryListItemStruct;
+
+                    XmlSitemapCount--;
+                }
+            }
+
+
+            // Set Content Struct
+            DataBaseSocket db = new DataBaseSocket();
+            DataBaseDataReader dbdr = new DataBaseDataReader();
+			dbdr.dr = db.GetProcedure("get_content", "@inner_attach", "where el_site.site_id = " + SiteId);
+
+            if ((XmlSitemapCount - CategoryNode.ChildNodes.Count) >= 0)
+            {
+                if (dbdr.dr != null && dbdr.dr.HasRows)
+                    while (dbdr.dr.Read())
+                    {
+                        // Set Extra Content Url Value
+                        ExtraValue evc = new ExtraValue();
+
+                        evc.SiteGlobalName = dbdr.dr["site_global_name"].ToString();
+                        evc.SiteName = dbdr.dr["site_name"].ToString();
+                        evc.ContentId = dbdr.dr["content_id"].ToString();
+                        evc.ContentTitle = dbdr.dr["content_title"].ToString();
+                        evc.CategoryId = dbdr.dr["category_id"].ToString();
+
+                        CategoryClass category = new CategoryClass();
+                        evc.ParrentCategory = category.GetParentCategory(dbdr.dr["site_global_name"].ToString(), dbdr.dr["category_id"].ToString());
+                        evc.CategoryName = category.CategoryName;
+
+
+                        TmpContentListItemStruct = ContentListItemStruct;
+
+                        TmpContentListItemStruct = TmpContentListItemStruct.Replace("$_asp site_url_path;", siteMainUrl);
+                        TmpContentListItemStruct = TmpContentListItemStruct.Replace("$_asp content_id;", dbdr.dr["content_id"].ToString());
+                        TmpContentListItemStruct = TmpContentListItemStruct.Replace("$_asp content_title;", dbdr.dr["content_title"].ToString());
+                        TmpContentListItemStruct = TmpContentListItemStruct.Replace("$_asp priority;", "0.15");
+
+                        TmpContentListItemStruct = TmpContentListItemStruct.Replace("$_asp extra_content_url_value;", evc.GetContentUrlValue());
+
+                        SumContentListItemStruct += TmpContentListItemStruct;
+
+                        XmlSitemapCount--;
+                    }
+
+                db.Close();
+            }
+
+            UrlBoxStruct = UrlBoxStruct.Replace("$_asp item;", SumPageListItemStruct + SumCategoryListItemStruct + SumContentListItemStruct);
+
+            HttpContext.Current.Response.Write(UrlBoxStruct);
+
+            // Set Xml Sitemap Cache
+            cc.Insert("disk", CacheKey, UrlBoxStruct, XmlSitemapCache);
+        }
+
+        private string CategoryLastMod(string CategoryId)
+        {
+            DataBaseSocket db = new DataBaseSocket();
+            DataBaseDataReader dbdr = new DataBaseDataReader();
+			dbdr.dr = db.GetProcedure("get_last_content_category_date_time", "@category_id", CategoryId);
+
+            string CategoryLastMod = "";
+
+            if (dbdr.dr == null || !dbdr.dr.HasRows)
+            {
+                CategoryLastMod = string.Format("{0:yyyy-MM-ddTHH:mm:sszzz}", DateTime.Parse(ElanatConfig.GetNode("date_and_time/site_birthday").InnerText));
+
+                db.Close();
+                return CategoryLastMod;
+            }
+
+            dbdr.dr.Read();
+
+            CategoryLastMod = string.Format("{0:yyyy-MM-ddTHH:mm:sszzz}", DateTime.Parse(dbdr.dr["content_date_create"].ToString() + " " + dbdr.dr["content_time_create"].ToString()));
+
+            db.Close();
+
+            return CategoryLastMod;
+        }
+
+        private string GetCategoryChangeFreq(string CategoryId)
+        {
+            DataBaseSocket db = new DataBaseSocket();
+            DataBaseDataReader dbdr = new DataBaseDataReader();
+			dbdr.dr = db.GetProcedure("get_three_last_content_category_date", "@category_id", CategoryId);
+
+            List<string> LastDate = new List<string>();
+
+            if (dbdr.dr != null && dbdr.dr.HasRows)
+                while (dbdr.dr.Read())
+                    LastDate.Add(dbdr.dr["content_date_create"].ToString());
+
+            db.Close();
+
+            if (LastDate.Count > 2)
+                return GetContentFreqTime(LastDate[0], LastDate[1], LastDate[2]);
+            if (LastDate.Count == 2)
+                return GetContentFreqTime(LastDate[0], LastDate[1], DateAndTime.GetDate("yyyy/MM/dd"));
+            if (LastDate.Count == 1)
+                return GetContentFreqTime(ElanatConfig.GetNode("date_and_time/site_birthday").InnerText, LastDate[0], DateAndTime.GetDate("yyyy/MM/dd"));
+
+            return "never";
+        }
+
+        private string GetSubCategoryPriority(string CategorySubCategoryCount)
+        {
+            float Space = float.Parse(CategorySubCategoryCount) + 1;
+            return "0." + Math.Round(30 + (50 / Space)).ToString();
+        }
+
+        private string GetContentFreqTime(string Date1, string Date2, string Date3)
+        {
+            DataBaseSocket db = new DataBaseSocket();
+            DataBaseDataReader dbdr = new DataBaseDataReader();
+			dbdr.dr = db.GetProcedure("get_content_date_range", new List<string>() { "@date1", "@date2", "@date3" }, new List<string>() { Date1, Date2, Date3 });
+
+            if (dbdr.dr == null || !dbdr.dr.HasRows)
+            {
+                db.Close();
+                return null;
+            }
+
+            dbdr.dr.Read();
+
+            DateTime DateT1 = Convert.ToDateTime(Date1);
+            DateTime DateT2 = Convert.ToDateTime(Date2);
+            DateTime DateT3 = Convert.ToDateTime(Date3);
+            DateTime DateNow = Convert.ToDateTime(DateAndTime.GetDate("yyyy/MM/dd"));
+
+            int TimeCount = Convert.ToInt32(dbdr.dr["date_range_count"].ToString());
+
+            long TimeDiff = DateT2.Subtract(DateT1).Days + DateT3.Subtract(DateT2).Days + DateNow.Subtract(DateT3).Days;
+
+            db.Close();
+
+            if (TimeCount > 24 && TimeDiff == 0)
+                return "always";
+
+            if (TimeCount > 24 && TimeDiff <= 2)
+                return "hourly";
+
+            if (TimeCount >= 3 && TimeDiff <= 2)
+                return "daily";
+
+            if (TimeCount >= 1 && TimeDiff <= 7)
+                return "weekly";
+
+            if (TimeCount >= 1 && TimeDiff <= 31)
+                return "monthly";
+
+            if (TimeCount >= 1 && TimeDiff <= 365)
+                return "yearly";
+
+            return "never";
+        }
+    }
+}
