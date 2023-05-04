068351f017ad3fb64ba45c4f242dfeed349e2149
elanatframework <111444759+elanatframework@users.noreply.github.com>
2023-05-04 03:52:35 +0330

    Add files via upload

---
+++ App_Data/elanat_system_data/empty_patern/addon_load_native_dll/Default.aspx.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.InteropServices;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace elanat
+{
+    public partial class AddOnLoadNativeDll : System.Web.UI.Page
+    {
+       protected void Page_Load(object sender, EventArgs e)
+        {
+            string FormString = Request.Form.AllKeys.ToString();
+            string QueryString = Request.QueryString.ToString();
+
+            Response.Write(NativeDll.NativeMethods.Main(HttpContext.Current.Server.MapPath("main.dll"), FormString, QueryString));
+        }
+    }
+}
