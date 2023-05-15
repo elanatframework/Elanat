969132047453e29e915a631b44bdfeb0681d3a30
elanatframework <111444759+elanatframework@users.noreply.github.com>
2023-05-15 23:27:19 +0330

    Update Default.aspx.cs

--- action/system_access/scheduled_tasks/empty_tmp_directory/Default.aspx.cs
+++ action/system_access/scheduled_tasks/empty_tmp_directory/Default.aspx.cs
@@ -15,14 +15,14 @@ namespace elanat
             DirectoryInfo DirInfo = new DirectoryInfo(HttpContext.Current.Server.MapPath(StaticObject.SitePath + "App_Data/tmp/"));
             foreach (DirectoryInfo dir in DirInfo.GetDirectories())
             {
-                if ((long.Parse(DateTime.Now.ToString("yyyyMMddHHmmss")) - long.Parse(dir.LastAccessTime.ToString("yyyyMMddHHmmss"))) >= 3600)
+                if ((long.Parse(DateTime.Now.ToString("yyyyMMddHHmmss")) - long.Parse(dir.LastAccessTime.ToString("yyyyMMddHHmmss"))) >= 36000)
                     Directory.Delete(HttpContext.Current.Server.MapPath(StaticObject.SitePath + "App_Data/tmp/" + dir.Name), true);
             }
 
             FileInfo[] fileInfo = DirInfo.GetFiles("*.*", SearchOption.TopDirectoryOnly);
             foreach (FileInfo file in fileInfo)
             {
-                if ((long.Parse(DateTime.Now.ToString("yyyyMMddHHmmss")) - long.Parse(file.LastAccessTime.ToString("yyyyMMddHHmmss"))) >= 3600)
+                if ((long.Parse(DateTime.Now.ToString("yyyyMMddHHmmss")) - long.Parse(file.LastAccessTime.ToString("yyyyMMddHHmmss"))) >= 36000)
                     File.Delete(HttpContext.Current.Server.MapPath(StaticObject.SitePath + "App_Data/tmp/" + file.Name));
             }
         }
