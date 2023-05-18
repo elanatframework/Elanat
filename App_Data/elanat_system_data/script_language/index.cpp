e351dcdd61a0868dac5427c89b679479d219697a
elanatframework <111444759+elanatframework@users.noreply.github.com>
2023-05-18 16:01:18 +0330

    Add files via upload

---
+++ App_Data/elanat_system_data/script_language/index.cpp
@@ -0,0 +1,12 @@
+﻿#include <iostream>
+#include <sstream>
+
+using namespace std;
+
+string QueryString; // Never Delete Or Edit It;
+string FormData; // Never Delete Or Edit It;
+
+int main()
+{
+	return 0;
+}
